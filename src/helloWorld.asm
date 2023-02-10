e7da548ba7850c392b6ad83174774e2406b4c59c
Josh Farias <116445946+joshfarias@users.noreply.github.com>
2023-02-09 21:15:01 -0500

    Create helloWorld.asm

---
+++ src/helloWorld.asm
@@ -0,0 +1,29 @@
+#################################################################
+#
+#	Program Name: helloWorld.asm
+# 
+#	Description: Basic text output
+#
+#	Language: MIPS ASM
+#
+#	Date: 1/28/2023
+# 
+#	Author: Joshua Farias
+#
+#################################################################
+
+.data
+
+message:	.asciiz	"Hello World\n" # this is the message we want to print
+
+.text
+
+main:
+
+la $a0, message # load the starting address of the message
+
+li $v0, 4 # load 4 into register $v0
+syscall # syscall(4) will print the string
+
+li $v0, 10 # exit
+syscall
